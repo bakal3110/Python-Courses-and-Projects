9123d5a2e28219c1e5e5f3a4d6a8833488ea697a
Bartosz Kaluski <125510397+bakal3110@users.noreply.github.com>
2025-09-10 16:24:41 +0200

    Insert character alignments and player amounts

---
+++ BoTC/role-distributor.py
@@ -0,0 +1,20 @@
+characterAlignment_dict = {
+    "Townsfolk" : ["Washerwoman", "Librarian", "Investigator", "Chef", "Empath", "Fortune Teller", "Undertaker", "Monk", "Ravenkeeper", "Virgin", "Virgin", "Slayer", "Soldier", "Mayor"],
+    "Outsiders" : ["Butler", "Drunk", "Recluse", "Saint"],
+    "Minions" : ["Poisoner", "Spy", "Scarlet Woman", "Baron"],
+    "Demons" : ["Imp"]
+}
+
+characterAmount_dict = {
+    5 : [3,0,1,1],
+    6 : [3,1,1,1],
+    7 : [5,0,1,1],
+    8 : [5,1,1,1],
+    9 : [5,2,1,1],
+    10 : [7,0,2,1],
+    11 : [7,1,2,1],
+    12 : [7,2,2,1],
+    13 : [9,0,3,1],
+    14 : [9,1,3,1],
+    15 : [9,2,3,1]
+}
