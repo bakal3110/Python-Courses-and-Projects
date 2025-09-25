926644c0e140eb933d8a403217a1720700cf3d54
Bartosz Kaluski <125510397+bakal3110@users.noreply.github.com>
2025-09-25 03:02:59 +0200

    Created Gantt Chart

---
+++ Gantt Chart/config/settings.py
@@ -0,0 +1,28 @@
+# data 
+DATE_FORMAT = '%Y-%m-%d'
+
+# chart 
+TITLE = "Super Cool Project"
+TITLE_SIZE = 12
+TITLE_FONT_WEIGHT = "bold"
+
+FONT_COLOR = "#6C6C6C"
+
+X_LABEL = ""
+Y_LABEL = ""
+LABEL_SIZE = 8
+
+DAY_FONT_SIZE = 8
+MONTH_FONT_SIZE = 10
+MONTH_FONT_WEIGHT = "bold"
+
+BAR_COLOR = "#30C7DC"
+TEAM_BAR_COLORS = {
+    "Technology": "#56778f",
+    "Business": "#91be6f",
+    "Finance": "#ff6e61",
+    "Marketing": "#f9c54e"
+}  
+
+FONT_FAMILY = "sans-serif"
+FONT_SANS_SERIF = ["Arial", "Roboto", "DejaVu Sans"]
