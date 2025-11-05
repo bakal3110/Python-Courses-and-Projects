4c89e465950c22b42ececf59a9d7112c5ecdd458
Bartosz Kaluski <125510397+bakal3110@users.noreply.github.com>
2025-11-05 10:58:16 +0100

    Updated main.py

--- Password Locker/main.py
+++ Password Locker/main.py
@@ -1,1 +1,22 @@
+#! python3
+# main.py - An insecure password locker program.
 
+PASSWORDS = {
+            'email': 'sdwqvdtds!@#!@SADd',
+            'ryanair': 'ryanaaairr22',
+            'youtube': 'ilikecats3009'
+            }
+
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+    print('Usage: python main.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print(f'Password for {account} copied to clipboard.')
+else:
+    print(f'There is no account named {account}')
